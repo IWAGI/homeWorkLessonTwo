4961049ec83006ce4a9249b22205d265a6986180
IWAGI <probel54@yandex.ru>
2022-02-10 12:05:19 +0700

    App refactored after check.

--- homeWorkLessonTwo/ViewController.swift
+++ homeWorkLessonTwo/ViewController.swift
@@ -11,77 +11,61 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var redValueLabel: UILabel!
-    @IBOutlet weak var greenValueLabel: UILabel!
-    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
-    
-    private var redValue: CGFloat = 50
-    private var greenValue: CGFloat = 75
-    private var blueValue: CGFloat = 100
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         colorView.layer.cornerRadius = 30
         
-        setupSliders()
-        startValuesOfElements()
+        setupMinTrackTintColorForSliders()
+        setColorForView()
+        setupLabelsValue(for: redLabel, greenLabel, blueLabel)
+    }
+
+    @IBAction func slidersValueChanged(_ sender: UISlider) {
+        setColorForView() // Set view color
+        
+        switch sender {
+        case redSlider: redLabel.text = string(from: sender)
+        case greenSlider: greenLabel.text = string(from: sender)
+        case blueSlider: blueLabel.text = string(from: sender)
+        default: break
+        }
     }
     
-    private func setupSliders() {
+    private func setupMinTrackTintColorForSliders() {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
     }
-
-    @IBAction func redSLiderValueChanged() {
-        redValueLabel.text = String(Int(redSlider.value))
-        
-        redValue = CGFloat(redSlider.value)
-        colorView.backgroundColor = UIColor(red: redValue / 255,
-                                            green: greenValue / 255,
-                                            blue: blueValue / 255,
+    
+    private func setColorForView() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
+                                            green: CGFloat(greenSlider.value) / 255,
+                                            blue: CGFloat(blueSlider.value) / 255,
                                             alpha: 1)
     }
     
-    @IBAction func greenSLiderValueChanged() {
-        greenValueLabel.text = String(Int(greenSlider.value))
-        
-        greenValue = CGFloat(greenSlider.value)
-        colorView.backgroundColor = UIColor(red: redValue / 255,
-                                            green: greenValue / 255,
-                                            blue: blueValue / 255,
-                                            alpha: 1)
+    private func setupLabelsValue(for labels: UILabel...) { //main - Set tags for labels and sliders
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabel.text = string(from: redSlider)
+            case 1: greenLabel.text = string(from: greenSlider)
+            case 2: blueLabel.text = string(from: blueSlider)
+            default: break
+            }
+        }
     }
     
-    @IBAction func blueSLiderValueChanged() {
-        blueValueLabel.text = String(Int(blueSlider.value))
-        
-        blueValue = CGFloat(blueSlider.value)
-        colorView.backgroundColor = UIColor(red: redValue / 255,
-                                            green: greenValue / 255,
-                                            blue: blueValue / 255,
-                                            alpha: 1)
-    }
-    
-    private func startValuesOfElements() {
-        redSlider.value = Float(redValue)
-        greenSlider.value = Float(greenValue)
-        blueSlider.value = Float(blueValue)
-        
-        colorView.backgroundColor = UIColor(red: redValue / 255,
-                                            green: greenValue / 255,
-                                            blue: blueValue / 255,
-                                            alpha: 1)
-        
-        redValueLabel.text = String(Int(redSlider.value))
-        greenValueLabel.text = String(Int(greenSlider.value))
-        blueValueLabel.text = String(Int(blueSlider.value))
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.0f", slider.value)
     }
     
 }
